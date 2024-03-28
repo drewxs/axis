d7e250bca3691830050fd17a42accab19c2ee56c
Andrew X. Shah <drewshah0@gmail.com>
2024-03-28 05:39:33 -0600

    feat: create window module base

--- engine_tester/src/main.rs
+++ engine_tester/src/main.rs
@@ -1,13 +1,15 @@
-use axis::custom_errors::Errors;
+use axis::graphics::window::Window;
 
 fn main() {
-    axis::logger::init();
-    error_test(1);
-}
+    let mut window = Window::new(1080, 720, "Window");
 
-fn error_test(num: i32) -> Result<(), Errors> {
-    if num == 1 {
-        Err(Errors::TestError.into());
+    window.init_gl();
+
+    while !window.should_close() {
+        unsafe {
+            gl::ClearColor(0.3, 0.5, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.update();
     }
-    Ok(())
 }
---
+++ src/graphics/window.rs
@@ -0,0 +1,54 @@
+use glfw::{
+    fail_on_errors, Action, Context, Glfw, GlfwReceiver, Key, PWindow, WindowEvent, WindowMode,
+};
+
+pub struct Window {
+    glfw: Glfw,
+    window_handle: PWindow,
+    events: GlfwReceiver<(f64, WindowEvent)>,
+}
+
+impl Window {
+    pub fn new(width: u32, height: u32, title: &str) -> Window {
+        let mut glfw = glfw::init(fail_on_errors).unwrap();
+
+        let (mut window, events) = glfw
+            .create_window(width, height, title, WindowMode::Windowed)
+            .expect("Failed to create GLFW window.");
+
+        window.set_framebuffer_size_polling(true);
+        window.set_key_polling(true);
+
+        Window {
+            glfw,
+            window_handle: window,
+            events,
+        }
+    }
+
+    pub fn init_gl(&mut self) {
+        self.window_handle.make_current();
+        gl::load_with(|s| self.window_handle.get_proc_address(s) as *const _);
+    }
+
+    pub fn should_close(&self) -> bool {
+        self.window_handle.should_close()
+    }
+
+    pub fn update(&mut self) {
+        self.process_events();
+        self.glfw.poll_events();
+        self.window_handle.swap_buffers();
+    }
+
+    fn process_events(&mut self) {
+        for (_, event) in glfw::flush_messages(&self.events) {
+            match event {
+                WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    self.window_handle.set_should_close(true);
+                }
+                _ => {}
+            }
+        }
+    }
+}
